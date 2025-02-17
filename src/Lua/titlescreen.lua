b36447901c0d0136fa3d5725ab51c7f412b5db13
Pacola <149804664+PacolaYes@users.noreply.github.com>
2025-02-17 14:47:46 -0300

    W.I.P titlescreen functionality
    makes it so the camera should be located where a blue crawla is at, the vertical angle of the camera is the camera's position looking at a red crawla's position or 0. title screen map SHOULD have the is5btitle map header thingie

--- src/Lua/titlescreen.lua
+++ src/Lua/titlescreen.lua
@@ -1,21 +1,38 @@
-local titlepos = 300 --this defines the initial position of the graphics
-local function mytitlescreen(v, player)
-	local w = v.width()
-	local h = v.height()
-	
-	local sky = v.cachePatch("TITLESKY")
-	
-	v.drawStretched(0, 0, FixedDiv(w, sky.width), FixedDiv(h, sky.height), sky, V_NOSCALEPATCH)
-	v.draw(titlepos,10,v.cachePatch("LOGO")) --draw the LOGO graphic with titlepos as the x axis 
-	v.draw(-titlepos,10,v.cachePatch("LOGO2")) --draw the LOGO2 graphic with negative titlepos as the x axis 
-	v.draw(0,-titlepos,v.cachePatch("SRB2T")) --draw the SRB2T  graphic with negative titlepos as the y axis
-	if titlepos > 0 --check if the titlepos is bigger than 0
-	titlepos = $ - 1*titlepos/4 --subtract the titlepos value to move it to the middle of the screen, until the check on the line above stops it
-	--the /4 makes an ease-out transition thing or something, i suck at math i just know it works
-	end
-end
 
-hud.add(mytitlescreen, "title") --add the mytitlescreen function defined before (line 2)
-addHook("PlayerThink", function(player) --this hook works when the player is in a level
-	titlepos = 300 -- reset the titlepos to 300
+local cameraMo
+
+addHook("ThinkFrame", function()
+	if gamestate ~= GS_TITLESCREEN
+	or not (mapheaderinfo[gamemap] and mapheaderinfo[gamemap].is5btitle) then
+		cameraMo = nil
+		return
+	end
+	
+	if not (cameraMo and cameraMo.valid) then
+		local aimingZ
+		for mo in mobjs.iterate() do
+			if mo.type == MT_BLUECRAWLA then
+				local cmo = P_SpawnMobj(mo.x, mo.y, mo.z, MT_THOK)
+				cmo.state = S_INVISIBLE
+				cmo.tics = -1
+				cmo.flags = MF_NOGRAVITY|MF_NOCLIP|MF_NOCLIPHEIGHT
+				cmo.angle = mo.angle
+				cameraMo = cmo
+				P_RemoveMobj(mo)
+			elseif mo.type == MT_REDCRAWLA then
+				aimingZ = mo.z
+				P_RemoveMobj(mo)
+			end
+		end
+		if aimingZ == nil then
+			cameraMo.srb5baiming = 0
+		else
+			cameraMo.srb5baiming = R_PointToAngle2(0, cameraMo.z, 0, aimingZ)
+		end
+	else
+		P_TeleportCameraMove(camera, cameraMo.x, cameraMo.y, cameraMo.z)
+		camera.angle = cameraMo.angle
+		camera.aiming = cameraMo.srb5baiming
+	end
 end)
+
