fb42a487755f3c2eb06ee4a95b97a2d16ff5054d
Pacola <149804664+PacolaYes@users.noreply.github.com>
2025-03-04 14:33:34 -0300

    make book & match be colorized with a fire flower or star
    also makes it so the supercolor thing applies to ghosts, just to make sure

--- src/Lua/super form stuff.lua
+++ src/Lua/super form stuff.lua
@@ -15,7 +15,13 @@ skincolors[SKINCOLOR_YOYLEMETAL] = {
 
 addHook("PlayerThink", function(p)
     if not (p.mo and p.mo.valid)
-	or not SRB5b_skinCheck(p.mo.skin) return end
+	or not SRB5b_skinCheck(p.mo.skin)
+		if p.bookcolorized then
+			p.bookcolorized = false
+			p.mo.colorized = false
+		end
+		return
+	end
 	
 	local skin = skins[p.mo.skin]
 	-- super form stuff
@@ -23,13 +29,31 @@ addHook("PlayerThink", function(p)
 	or (p.mo.color >= skin.supercolor and p.mo.color <= skin.supercolor+4) then
 		p.mo.color = SKINCOLOR_YOYLEMETAL
 	end
+	
+	if p.mo.skin ~= "icecube"
+	and ( (p.powers[pw_shield] & SH_FIREFLOWER)
+	or mapheaderinfo[gamemap]
+	and (mapheaderinfo[gamemap].typeoflevel & TOL_MARIO)
+	and p.powers[pw_invulnerability] ) then
+		local header = mapheaderinfo[gamemap]
+		
+		p.bookcolorized = true
+		p.mo.colorized = true
+		p.mo.eflags = $|MFE_FORCESUPER
+	elseif p.bookcolorized then
+		p.mo.colorized = false
+		p.bookcolorized = false
+	end
 end)
 
 addHook("MobjThinker", function(mo)
-	if (mo.target and mo.target.valid)
-	and SRB5b_skinCheck(mo.target.skin)
-	and mo.target.player.powers[pw_super]
-	and mo.color ~= SKINCOLOR_YOYLEMETAL
+	if not (mo.target and mo.target.valid)
+	or not SRB5b_skinCheck(mo.target.skin) then return end
+	
+	local skin = skins[mo.target.skin]
+	if ( mo.target.player.powers[pw_super]
+	or mo.target.color >= skin.supercolor and mo.target.color <= skin.supercolor+4 )
+	and mo.color ~= SKINCOLOR_YOYLEMETAL then
 		mo.color = SKINCOLOR_YOYLEMETAL
 		return
 	end
