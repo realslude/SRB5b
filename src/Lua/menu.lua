ca37a480475c74609302378925c07755fd716658
Sludesus <98156333+realslude@users.noreply.github.com>
2025-05-02 14:52:31 +0200

    funny menu.lua
    <@839597282012823562> https://tenor.com/view/mario-super-mario-rpg-remake-gloomy-sad-gif-9905453258393209659

---
+++ src/Lua/menu.lua
@@ -0,0 +1,58 @@
+/*
+Future LUA for the menu code
+will write titles and descriptions 
+
+template;
+cmd name: 
+icon file name: 
+title: 
+Description: 
+
+cmd name: book_grabanything
+icon file name: MENU_GRABANYTHING
+title: Grab anything
+Description:
+Allows the player to grab whatever "thing" that they want
+(E.g. rings, bosses, trees, Braks electric shield /srs)
+
+cmd name: book_grabweight
+icon file name: MENU_GRABWEIGHT
+title: Object weight
+Description: 
+Determines if the object that you are holding should have weight.
+The larger the object the more weight it would have.
+Weight affects how high your jump is
+
+cmd name: book_grabbrain
+icon file name: MENU_GRABBRAIN
+title: Carried object AI
+Description: 
+Allows for the object that you are carrying to function normally
+(E.g. a carried turret will still shoot)
+
+cmd name: book_grabgrief
+icon file name: MENU_GRABGRIEF
+title: Player pickup
+Description: 
+Determines wherever players should be allowed to be carried
+
+cmd name: book_deadlythrow
+icon file name: MENU_DEADLYTHROW
+title: Deadly throw
+Description: 
+Determines if the thrown object should instantly kill whatever it lands on
+
+cmd name: book_throwaccel
+icon file name: X
+title: Throw acceleration
+Description: 
+Allows for the thrown object to be thrown faster
+(If disabled then thrown objects will move similar to how they did pre-rewrite)
+
+cmd name: book_keepinventory
+icon file name: MENU_KEEPINVENTORY
+title: Keep object
+Description: 
+Allows you to keep whatever item that you are holding between levels 
+
+*/
